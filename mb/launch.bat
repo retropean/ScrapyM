9cef31c5edc3e5cda9b7d85559276c3c704857fc
Eric Randall <eric.randalls@gmail.com>
2014-06-13 15:14:32 -0400

    Batch file update
    Still may not go with this in the long term, but worth testing this
    weekend.

--- mb/launch.bat
+++ mb/launch.bat
@@ -1,1 +1,7 @@
 scrapy crawl mb -o test.csv -t csv
+SLEEP 86400
+scrapy crawl mb -o test2.csv -t csv
+SLEEP 86400
+scrapy crawl mb -o test3.csv -t csv
+SLEEP 86400
+scrapy crawl mb -o test4.csv -t csv
